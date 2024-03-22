426aea1af230442ad39a23955fe6c82c869f0c0e
aishalobaton <116447601+aishalobaton@users.noreply.github.com>
2024-03-22 16:35:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,22 +17,22 @@ st.subheader("Por favor ingresa una foto del texto que quieres analizar")
 translator = Translator()
 tld="es"
 
- def generar_audio():
-    if x >= 0.5:
-        texto_audio = "Yupi, tu texto feliz"
-        tts = gTTS(text=texto_audio, lang='es')
-        tts.save("feliz.mp3")
-        os.system("mpg123 feliz.mp3")
-    elif x <= -0.5:
-        texto_audio = "Que mal, tu texto es triste"
-        tts = gTTS(text=texto_audio, lang='es')
-        tts.save("triste.mp3")
-        os.system("mpg123 triste.mp3")
-    else:
-        texto_audio = "Tu texto es neutral"
-        tts = gTTS(text=texto_audio, lang='es')
-        tts.save("neutro.mp3")
-        os.system("mpg123 neutro.mp3")
+def generar_audio():
+ if x >= 0.5:
+  texto_audio = "Yupi, tu texto feliz"
+  tts = gTTS(text=texto_audio, lang='es')
+  tts.save("feliz.mp3")
+  os.system("mpg123 feliz.mp3")
+ elif x <= -0.5:
+  texto_audio = "Que mal, tu texto es triste"
+  tts = gTTS(text=texto_audio, lang='es')
+  tts.save("triste.mp3")
+  os.system("mpg123 triste.mp3")
+ else:
+  texto_audio = "Tu texto es neutral"
+  tts = gTTS(text=texto_audio, lang='es')
+  tts.save("neutro.mp3")
+  os.system("mpg123 neutro.mp3")
 
 
     
