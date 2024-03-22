49d3c1868fd1cffb173ea4261046a6cacdba29e2
aishalobaton <116447601+aishalobaton@users.noreply.github.com>
2024-03-22 17:12:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ from googletrans import Translator
 import time
 import glob
 from gtts import gTTS
-
-
+from textblob import TextBlob
 
 st.title('Las palabras y las emociones')
 image = Image.open('emoticones.jpg')
@@ -14,66 +13,55 @@ st.image(image)
 st.subheader("Al ingresar un texto se generará un audio con lo que escribiste y además se analizará el tipo de emoción que se incluye en el texto.")
 
 translator = Translator()
-try:
-    os.mkdir("temp")
-except:
-    pass
+tld="es"
 
-text = st.text_input("Ingrese el texto.")
-
-# Selección del idioma de traducción
-languages = {
-    "es": "Español",
-    "en": "Inglés",
-    "fr": "Francés",
-    "de": "Alemán",
-    "it": "Italiano",
-    "ja": "Japonés",
-    "ko": "Coreano",
-    "zh-cn": "Chino",
-    "ru": "Ruso",
-}
-input_language = es
-output_language = st.selectbox("¿A qué idioma deseas traducir?:", list(languages.values()))
-
-def text_to_speech(text, input_language, output_language):
-    translation = translator.translate(text, src=input_language, dest=output_language)
-    trans_text = translation.text
-    tts = gTTS(trans_text, lang=output_language, slow=False)
+def text_to_speech(text, tld):
+    tts = gTTS(text, lang="es", slow=False)
     try:
         my_file_name = text[0:20]
     except:
         my_file_name = "audio"
     tts.save(f"temp/{my_file_name}.mp3")
-    return my_file_name, trans_text
+    return my_file_name, text
+
+text = st.text_input("Ingrese el texto.")
 
 if st.button("Audio"):
-    result, output_text = text_to_speech(text, input_language[:2], output_language[:2])
+    result, output_text = text_to_speech(text, tld)
     audio_file = open(f"temp/{result}.mp3", "rb")
     audio_bytes = audio_file.read()
     st.markdown(f"## Tú audio:")
     st.audio(audio_bytes, format="audio/mp3", start_time=0)
 
 with st.expander('Analizar texto'):
+
     if text:
-        translation = translator.translate(text, src=input_language[:2], dest="en")
+        dest_lang = st.selectbox(
+            "Selecciona el idioma de destino:",
+            ("Inglés", "Español", "Francés", "Alemán", "Italiano", "Portugués", "Ruso", "Chino", "Japonés", "Coreano")
+        )
+        dest_lang_code = dest_lang.lower()[:2]  # Obtener el código de idioma
+
+        translation = translator.translate(text, dest=dest_lang_code)
         trans_text = translation.text
         blob = TextBlob(trans_text)
-        st.write('Polaridad: ', round(blob.sentiment.polarity,2))
-        st.write('Subjetividad: ', round(blob.sentiment.subjectivity,2))
-        x=round(blob.sentiment.polarity,2)
+        st.write('Texto traducido:', trans_text)
+        st.write('Polarity:', round(blob.sentiment.polarity, 2))
+        st.write('Subjectivity:', round(blob.sentiment.subjectivity, 2))
+
+        x = round(blob.sentiment.polarity, 2)
         if x >= 0.5:
-            st.write( 'Es un sentimiento Positivo 😊')
+            st.write('Es un sentimiento Positivo 😊')
             image_feliz = Image.open('feliz.jpg')
             st.image(image_feliz)
 
         elif x <= -0.5:
-            st.write( 'Es un sentimiento Negativo 😔')
+            st.write('Es un sentimiento Negativo 😔')
             image_triste = Image.open('triste.jpg')
             st.image(image_triste)
 
         else:
-            st.write( 'Es un sentimiento Neutral 😐')
+            st.write('Es un sentimiento Neutral 😐')
             image_neutro = Image.open('neutro.jpg')
             st.image(image_neutro)
 
@@ -88,3 +76,4 @@ def remove_files(n):
                 print("Deleted ", f)
 
 remove_files(7)
+
